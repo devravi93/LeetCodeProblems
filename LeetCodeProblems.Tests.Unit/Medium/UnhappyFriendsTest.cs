a3978fa4c2180a719d194e2c2d14f0f3994a487a
Ravi <dev.raviraghav93@gmail.com>
2023-05-11 11:53:41 +0530

    Unhappy Friends problem - Medium
    Unhappy Friends problem - Medium

---
+++ LeetCodeProblems.Tests.Unit/Medium/UnhappyFriendsTest.cs
@@ -0,0 +1,97 @@
+﻿using LeetCodeProblems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Tests.Unit.Medium
+{
+    public class UnhappyFriendsTest
+    {
+        [Fact]
+        public void Success()
+        {
+            UnhappyFriends unhappyFriends = new UnhappyFriends();
+            int n = 4;
+            var preferences = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 3, 2, 0 },
+                new int[] { 3, 1, 0 },
+                new int[] { 1, 2, 0 }
+            };
+
+            var pairs = new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 2, 3 }
+            };
+            var result = unhappyFriends.Process(n, preferences, pairs);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Success_2()
+        {
+            UnhappyFriends unhappyFriends = new UnhappyFriends();
+            int n = 2;
+            var preferences = new int[][]
+            {
+                new int[] { 1 },
+                new int[] { 0 },
+            };
+
+            var pairs = new int[][]
+            {
+                new int[] { 1, 0 }
+            };
+            var result = unhappyFriends.Process(n, preferences, pairs);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Success_3()
+        {
+            UnhappyFriends unhappyFriends = new UnhappyFriends();
+            int n = 4;
+            var preferences = new int[][]
+             {
+                new int[] { 1, 3, 2 },
+                new int[] { 2, 3, 0 },
+                new int[] { 1, 3, 0 },
+                new int[] { 0, 2, 1 }
+             };
+
+            var pairs = new int[][]
+            {
+                new int[] { 1, 3 },
+                new int[] { 0, 2 }
+            };
+            var result = unhappyFriends.Process(n, preferences, pairs);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Success_4()
+        {
+            UnhappyFriends unhappyFriends = new UnhappyFriends();
+            int n = 4;
+            var preferences = new int[][]
+             {
+                new int[] { 1, 3, 2 },
+                new int[] { 2, 3, 0 },
+                new int[] { 1, 0, 3 },
+                new int[] { 1, 0, 2 }
+             };
+
+            var pairs = new int[][]
+            {
+                new int[] { 2, 1 },
+                new int[] { 3, 0 }
+            };
+            var result = unhappyFriends.Process(n, preferences, pairs);
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ LeetCodeProblems/Medium/UnhappyFriends.cs
@@ -0,0 +1,91 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/count-unhappy-friends/
+    /// Problem Statement : You are given a list of preferences for n friends, where n is always even.
+    /// For each person i, preferences[i] contains a list of friends sorted in the order of preference.In other words, 
+    /// a friend earlier in the list is more preferred than a friend later in the list. Friends in each list are denoted by integers from 0 to n-1.
+    /// All the friends are divided into pairs.The pairings are given in a list pairs, where pairs[i] = [xi, yi] denotes xi is paired with yi and yi is paired with xi.
+    /// However, this pairing may cause some of the friends to be unhappy. A friend x is unhappy if x is paired with y and there exists a friend u who is paired with v but:
+    /// x prefers u over y, and u prefers x over v. Return the number of unhappy friends.
+    /// </summary>
+    public class UnhappyFriends
+    {
+        public int Process(int n, int[][] preferences, int[][] pairs)
+        {
+            int unHappy = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var pref = preferences[i];
+                var pair = findPair(pairs, i);
+                var otherIndividual = pair.First(t => t != i);
+                var otherPref = preferences[otherIndividual];
+
+                var indexOfOther = pref.ToList().IndexOf(otherIndividual);
+                if (indexOfOther == 0)
+                    continue;
+
+                if (indexOfOther == -1)
+                {
+                    unHappy++;
+                    continue;
+                }
+
+                var preferedFriends = pref.Take(new Range(0, indexOfOther));
+
+                foreach (var preferedFriend in preferedFriends)
+                {
+                    var friendsPref = preferences[preferedFriend];
+                    var friendsPair = findPair(pairs, preferedFriend);
+                    var friendsOtherIndividual = friendsPair.First(t => t != preferedFriend);
+                    var friendsIndexOfOther = friendsPref.ToList().IndexOf(friendsOtherIndividual);
+
+                    if (friendsIndexOfOther == 0)
+                        continue;
+
+                    var indexOfCurrent = friendsPref.ToList().IndexOf(i);
+                    if (indexOfCurrent < friendsIndexOfOther)
+                    {
+                        unHappy++;
+                        break;
+                    }
+                }
+            }
+
+            //for (int i = 0; i < pairs.Length; i++)
+            //{
+            //    var pair = pairs[i];
+            //    var prefPrimary = preferences[pair[0]];
+            //    var prefSecondary = preferences[pair[1]];
+            //    var indexPair1 = prefPrimary.ToList().IndexOf(pair[1]);
+            //    var indexPair2 = prefSecondary.ToList().IndexOf(pair[1]);
+            //    if (indexPair1 > -1)
+            //    {
+            //        var friendsPreffered1 = prefPrimary.Take(new Range(0, indexPair1));
+            //    }
+            //    else
+            //        unHappy++;
+
+
+            //    //if (prefPrimary[0] != pair[1])
+            //    //    unHappy++;
+            //    //if (prefSecondary[0] != pair[0])
+            //    //    unHappy++;
+            //}
+
+            return unHappy;
+        }
+
+        private int[] findPair(int[][] pairs, int i)
+        {
+            foreach (var pair in pairs)
+            {
+                if (pair[0] == i || pair[1] == i)
+                    return pair;
+            }
+
+            return new int[] { };
+        }
+    }
+}
